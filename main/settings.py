a282fbb68f8b2c504981b71c14a4eb1bb9087e70
Ciaran Griffin <griffinciaran3@gmail.com>
2024-06-12 09:46:04 +0000

    fix env

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriffi-recipe2024-do4x76w4yzz.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriffi-recipe2024-do4x76w4yzz.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
