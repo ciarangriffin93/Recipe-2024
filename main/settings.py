b5b9d22588b011d3e37e835687a9c88a1df6f67c
Ciaran Griffin <griffinciaran3@gmail.com>
2024-06-12 09:17:20 +0000

    readies code for deploy

--- main/settings.py
+++ main/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriffi-recipe2024-0q37v1cmew1.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ciarangriffi-recipe2024-do4x76w4yzz.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
