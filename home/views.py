474cd50e363d1d2f79854a06fb9712098b574fdc
Ciaran Griffin <griffinciaran3@gmail.com>
2024-06-13 19:31:37 +0000

    Wrapping up the home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from recipes.models import Recipe
 
 
 class Index(TemplateView):
     template_name = 'home/index.html'
+    model = Recipe
+    context_object_name = 'recipes'
+
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
