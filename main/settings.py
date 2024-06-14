9a8c190317a1424a7d85f8290bccc938e2d188d1
Ciaran Griffin <griffinciaran3@gmail.com>
2024-06-14 18:33:40 +0000

    enable serving of static files

--- main/settings.py
+++ main/settings.py
@@ -183,7 +183,7 @@ LOGIN_REDIRECT_URL = '/'
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Cloudinary Settings
